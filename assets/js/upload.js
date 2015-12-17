afdf09c17f56fbf064e529fe933fa29f06139429
Milton <tropa9527@gmail.com>
2015-12-17 02:10:13 +0000

    Added a description modal
    The description dialog box shows when the description button is clicked.

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,3 +1,16 @@
+window.onload = function () {
+	chooseUploadCategory();
+	// Add available years in the year picker
+	var currentYear = (new Date()).getFullYear();
+	for (var year = currentYear; year >= 1900; year--) {
+		var option = new Option(year, year);
+		$('#yearpicker').append($(option));
+	}
+};
+
+/**
+ * This method changes the allowed extensions dynamically depending on what category the user chosses.
+ */
 function chooseUploadCategory()
 {
 	var categories = document.getElementById("categories");
@@ -20,13 +33,22 @@ function getAcceptedExtensions(selectedCategory) {
 	}
 }
 
-window.onload = function () {
-	chooseUploadCategory();
+/**
+ * This method displays an error message if the user has not filled in all the boxes.
+ */
+function uploadButtonClick() {
+	var author = document.getElementById("author").value;
+	var year = document.getElementById("yearpicker").value;
+	var description = document.getElementById("description").value;
+	var restriction = document.getElementById("restriction").value;
+	var authorInformation = document.getElementById("author-information").value;
 
-	// Add available years in the year picker
-	var currentYear = (new Date()).getFullYear();
-	for (var year = currentYear; year >= 1900; year--) {
-		var option = new Option(year, year);
-		$('#yearpicker').append($(option));
+	if (isNullOrEmpty(author) || isNullOrEmpty(year) || isNullOrEmpty(description) ||
+			isNullOrEmpty(restriction) || isNullOrEmpty(authorInformation)) {
+		alert("Please fill in all the boxes before uploading a document.");
 	}
-};
+}
+
+function isNullOrEmpty(s) {
+	return (s === null || s === "");
+}
